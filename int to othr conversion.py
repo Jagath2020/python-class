de9d4ab5d2be53c7b029fd4fd4eac93ed3419979
Jagath2020 <97905943+Jagath2020@users.noreply.github.com>
2022-01-31 22:42:43 +0530

    Add files via upload

---
+++ int to othr conversion.py
@@ -0,0 +1,22 @@
+#data type type conversion int to float/bool/str
+
+#int value assignment
+val = 10
+
+#int to float conversion
+val1 = float(val)
+#print the float output
+print("The float output is:", val1)
+print(type(val1))
+
+#int to bool conversion
+val1 = bool(val)
+#print the bool output
+print("The bool output is:", val1)
+print(type(val1))
+
+#int to str conversion
+val1 = str(val)
+#print the str output
+print("The str output is:", val1)
+print(type(val1))
---
+++ perimetr.py
@@ -0,0 +1,9 @@
+#perimeter of circle
+
+#variable assignment
+pi=3.14
+radius=float(input("enter the input of radius:"))
+perimeter=2*pi*radius
+output=int(perimeter)
+#print the output
+print("perimeter of circle is",output)
