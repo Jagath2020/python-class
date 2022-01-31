b6293e7bd1c881ca6a1b39da6e86475b3bc012b6
Jagath2020 <97905943+Jagath2020@users.noreply.github.com>
2022-01-31 22:55:11 +0530

    Delete perimetr.py

--- perimetr.py
+++
@@ -1,9 +0,0 @@
-#perimeter of circle
-
-#variable assignment
-pi=3.14
-radius=float(input("enter the input of radius:"))
-perimeter=2*pi*radius
-output=int(perimeter)
-#print the output
-print("perimeter of circle is",output)
