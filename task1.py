0cce930cd5ea448dcae5e7a307687064bf21dd51
Jagath2020 <97905943+Jagath2020@users.noreply.github.com>
2022-02-19 16:07:03 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,19 @@
+#task1
+
+#Using Range function  print multiples of 5 from 0 to 75
+print("mutiple of 5 is :",list(range(0,75,5)))
+#Using Range function  print multiples of 8 from 0 to 72
+print("mutiple of 8 is :",list(range(0,72,8)))
+#Using Range function  print multiples of 5 from 75 to 0
+print("mutiple of 5 in reverse is :",list(range(75,0,-5)))
+#Using Range function  print multiples of 8 from 96 to 72     
+print("mutiple of 8 in reverse is :",list(range(96,72,-8)))
+
+
+
+
+
+
+
+
+    
